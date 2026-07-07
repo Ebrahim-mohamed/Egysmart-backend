e65dcc93645f6e680cada8c56db9099399515a97
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2026-07-07 15:54:24 +0300

    make db local on server

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(
-    "mongodb+srv://egysmart:LGLr7fINFRiXdABW@egysmart.ur5kj.mongodb.net/?appName=egysmart",
-  );
+  await mongoose.connect("mongodb://127.0.0.1:27017/egysmart");
   console.log("MongoDB connected");
 };
